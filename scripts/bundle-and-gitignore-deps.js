ffdc311f2ae478358b227bcfc6db3d0846a4be85
Yaminj <90813969+Yaminj@users.noreply.github.com>
2021-09-16 05:23:46 +0200

    Add files via upload

---
+++ scripts/bundle-and-gitignore-deps.js
@@ -0,0 +1,54 @@
+const Arborist = require('@npmcli/arborist')
+const { resolve } = require('path')
+const ignore = resolve(__dirname, '../node_modules/.gitignore')
+const { writeFileSync } = require('fs')
+const pj = resolve(__dirname, '../package.json')
+const pkg = require(pj)
+const bundle = []
+const arb = new Arborist({ path: resolve(__dirname, '..') })
+const shouldIgnore = []
+
+arb.loadVirtual().then(tree => {
+  for (const node of tree.children.values()) {
+    if (node.dev || node.isLink) {
+      console.error('ignore', node.name)
+      shouldIgnore.push(node.name)
+    } else if (tree.edgesOut.has(node.name)) {
+      console.error('BUNDLE', node.name)
+      bundle.push(node.name)
+    }
+  }
+  pkg.bundleDependencies = bundle.sort((a, b) => a.localeCompare(b, 'en'))
+
+  const ignores = shouldIgnore.sort((a, b) => a.localeCompare(b, 'en'))
+    .map(i => `/${i}`)
+    .join('\n')
+  const ignoreData = `# Automatically generated to ignore dev deps
+/.package-lock.json
+package-lock.json
+CHANGELOG*
+README*
+.editorconfig
+.idea/
+.npmignore
+.eslintrc*
+.travis*
+.github
+.jscsrc
+.nycrc
+.istanbul*
+.eslintignore
+.jshintrc*
+.prettierrc*
+.jscs.json
+.dir-locals*
+.coveralls*
+.babelrc*
+.nyc_output
+.gitkeep
+
+${ignores}
+`
+  writeFileSync(ignore, ignoreData)
+  writeFileSync(pj, JSON.stringify(pkg, 0, 2) + '\n')
+})
